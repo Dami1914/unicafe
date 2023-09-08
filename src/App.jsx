0d1b2c3c451f9123fe0dd395b5108b6d8ca6d79e
Damilola Jibowu <Aesthetico.design@gmail.com>
2023-09-08 16:00:48 +0100

    part1 1.10 commit refactoring exercise in unicafe exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
 
+function Button({updateState,good,bad,neutral}){
+
+  return(
+    <>
+      <button onClick={()=>{updateState(prev=>{ return {...prev,["good"]:good + 1}})}}>good</button>
+      <button onClick={()=>{updateState(prev=>{ return {...prev,["neutral"]:neutral + 1}})}} >neutral</button>
+      <button onClick={()=>{updateState(prev=>{ return {...prev,["bad"]:bad + 1}})}} >bad</button>
+    </>
+  )
+}
+
+function StatiticsLine({value,text}){
+
+  return(
+    <>
+      <div>{text} {value} </div>
+    </>
+  )
+}
+
 function Statitics({good,bad,neutral,average,positive,all}){
   return(
       <>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {average}</div>
-      <div>positive {positive}</div>
+        <StatiticsLine text="good" value={good}/>
+        <StatiticsLine text="neutral" value={neutral}/>
+        <StatiticsLine text="bad" value={bad}/>
+        <StatiticsLine text="all" value={all}/>
+        <StatiticsLine text="average" value={average}/>
+        <StatiticsLine text="positive" value={positive}/>
       </>
   )
 }
@@ -27,19 +47,16 @@ function App() {
   return (
     <>
     <div>
-    <h1>give feedback</h1>
-      <button onClick={()=>{setState(prev=>{ return {...prev,["good"]:state.good + 1}})}}>good</button>
-      <button onClick={()=>{setState(prev=>{ return {...prev,["neutral"]:state.neutral + 1}})}} >neutral</button>
-      <button onClick={()=>{setState(prev=>{ return {...prev,["bad"]:state.bad + 1}})}} >bad</button>
+      <h1>give feedback</h1>
+      <Button good={state.good} updateState={setState} neutral={state.neutral} bad={state.bad}/>
     </div>
     <div>
     <h1>Statitics</h1>
       { all < 1 ? 
       <h2>No feedback given</h2>:
-      <Statitics good={state.good} neutral={state.neutral} bad={state.bad} all={all} positive={positive} average={average}/>
+      <Statitics good={state.good} updateState={setState} neutral={state.neutral} bad={state.bad} all={all} positive={positive} average={average}/>
       }
     </div>
-      
     </>
   )
 }
